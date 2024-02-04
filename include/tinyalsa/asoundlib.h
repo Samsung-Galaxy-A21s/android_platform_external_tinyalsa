ec9883815109993643b685c316fcbdaf1fe3ddfa
RiskyGUY22 <thomas_turner@talktalk.net>
2024-02-04 21:08:47 +0000

    tinyalsa: fix compilation errors
    some weird conflight with hardware_samsung_slsi-linaro
    
    Change-Id: Icbb9bbf9a684a5a2779ecbc946a632f76f5bce86

--- include/tinyalsa/asoundlib.h
+++ include/tinyalsa/asoundlib.h
@@ -342,7 +342,6 @@ int mixer_ctl_get_range_max(struct mixer_ctl *ctl);
 int mixer_subscribe_events(struct mixer *mixer, int subscribe);
 int mixer_wait_event(struct mixer *mixer, int timeout);
 int mixer_consume_event(struct mixer *mixer);
-int mixer_read_event(struct mixer *mixer, struct ctl_event *ev);
 
 #if defined(__cplusplus)
 }  /* extern "C" */
